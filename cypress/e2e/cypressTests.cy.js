4fb23fad3c0f78140dfd4a6367dc52c236f24c03
Rakesh Roby <rakesh.roby@dyedurham.com>
2023-11-09 01:35:05 -0500

    fix

--- cypress/e2e/cypressTests.cy.js
+++ cypress/e2e/cypressTests.cy.js
@@ -26,9 +26,9 @@ describe('example to-do app', () => {
     cy.contains('About us').should('be.visible')
   })
 
-  it('User is able to click on “Install” and then on “npm install cypress” and make sure the copied text is “npm install cypress --save-dev”', () => {
-    cy.get('astro-island > .border').click();
-    cy.get('.slide-out-enter-to > .whitespace-nowrap').click();
+  it.only('User is able to click on “Install” and then on “npm install cypress” and make sure the copied text is “npm install cypress --save-dev”', () => {
+    cy.get('astro-island > .border').click({force:true});
+    cy.get('[data-cy="modal-install-copy"]').click({force: true});
     cy.window().then((win) => {
       win.navigator.clipboard.readText().then((text) => {
         expect(text).to.eq('npm install cypress --save-dev');
@@ -38,7 +38,7 @@ describe('example to-do app', () => {
 
   it('User is able to click on “Product” and then “visual review”', () => {
     cy.get('#dropdownProduct').trigger('mouseover')
-    cy.get(':nth-child(5) > .grid > :nth-child(2) > .group > .font-secondary').click();
+    cy.get(':nth-child(5) > .grid > :nth-child(2) > .group > .font-secondary').click({force: true});
     cy.url().should('include', '#visual_reviews');
     cy.xpath('//*[@id="visual_reviews"]/div/div/p')
       .should('be.visible')
@@ -46,7 +46,7 @@ describe('example to-do app', () => {
 
   it('User is able to click on “Product”, then “Smart Orchestration”, then scroll down to “Test Analytics” and see that the green circle is around “Test Analytics”', () => {
     cy.get('[data-cy="dropdown-product"]').trigger('mouseover')
-    cy.contains('Smart Orchestration').click()
+    cy.contains('Smart Orchestration').click({force: true})
     cy.xpath('/html/body/astro-island/header/astro-slot/astro-island/nav/div[2]/div[2]/a[3]')
       .should('not.have.class', 'whitespace-nowrap rounded-full border px-[12px] py-[4px] text-[16px] leading-[24px] transition-colors border-jade-200 bg-white text-teal-600')
     cy.xpath('//*[@id="test_analytics"]/div/div/p').scrollIntoView()
